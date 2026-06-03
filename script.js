c361a1dfc0d5e99a368c0e8b6586321816b7ef2c
Pedro Barão <predrobarao183@gmail.com>
2026-06-02 21:24:47 -0300

    a

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ var mobileMenu   = document.getElementById('mobileMenu');
 var mobileOverlay = document.getElementById('mobileOverlay');
 var mobileCloseBtn = document.getElementById('mobileCloseBtn');
 var menuLinks = document.querySelectorAll('.menu-link');
+var botaoModo = document.getElementById("mode-badge");
+const temaSalvo = localStorage.getItem("tema");
+
 
 function openMenu() {
   mobileMenu.classList.add('open');
@@ -18,6 +21,32 @@ function closeMenu() {
   hamburgerBtn.classList.remove('open');
   document.body.style.overflow = '';
 }
+
+if (temaSalvo === "escuro") {
+
+  body.classList.add("dark-mode");
+
+}
+
+botaoModo.addEventListener("click", function()
+{
+
+    document.body.classList.toggle('dark-mode');
+
+    if (document.body.classList.contains('dark-mode')) 
+    {
+
+        botaoModo.textContent = 'Modo Escuro 🌑';
+
+    } 
+    else 
+    {
+
+        botaoModo.textContent = 'Modo Claro ☀️';
+
+    }
+
+});
 
 hamburgerBtn.addEventListener('click', function() {
   if (mobileMenu.classList.contains('open')) closeMenu();
